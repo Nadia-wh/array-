d0d829c987fa1b6b6f269a6b3ac57de652a2c741
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2022-09-02 05:56:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//           0   1  2  3  4  5  6  7  8  9
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+  for (var i = 0 ;i < marks.length;i++){
+    if(marks[i] > 40){
+      console.log(marks[i])
+    }
+  }
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
